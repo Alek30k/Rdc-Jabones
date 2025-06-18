c876d168b24ff2b79b6f39674bbce29e265440ce
Alek30k <alemank23k@gmail.com>
2025-06-17 23:16:40 -0300

    last commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           toastOptions={{
             style: {
               background: "#000000",
-              color: "#fff",
+              color: "#f98e34",
             },
           }}
         />
