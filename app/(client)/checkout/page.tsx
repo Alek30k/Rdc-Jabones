414ab197f90db6a544a1fa679b16ca0dd2cfb0e4
Alek30k <alemank23k@gmail.com>
2025-06-22 23:51:05 -0300

    last commit

--- app/(client)/checkout/page.tsx
+++ app/(client)/checkout/page.tsx
@@ -5,6 +5,7 @@ import PriceFormatter from "@/components/PriceFormatter";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
 import {
   ArrowLeft,
@@ -22,7 +23,7 @@ import toast from "react-hot-toast";
 
 interface CheckoutData {
   items: Array<{
-    product: any;
+    product: Product;
   }>;
   customer: {
     name: string;
