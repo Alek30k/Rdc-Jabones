9bd841227cd6602ced2c647bbd69ba3d1fc178ea
Alek30k <alemank23k@gmail.com>
2025-06-25 23:08:58 -0300

    last commit

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -88,7 +88,7 @@ const SearchBar = () => {
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
         <input
           type="text"
-          placeholder="Buscar el regalo perfecto.."
+          placeholder="Buscar el regalo perfecto..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           onKeyDown={handleKeyDown}
