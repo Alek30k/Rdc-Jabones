6b0461f3d516b723b04ee9e14a3698dfacb475f6
Alek30k <alemank23k@gmail.com>
2025-06-26 16:26:46 -0300

    last commit

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,19 +1,19 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
-// import HomeCategories from "@/components/HomeCategories";
+import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
-// import { getCategories } from "@/sanity/queries";
+import { getCategories } from "@/sanity/queries";
 
 import React from "react";
 
 const Home = async () => {
-  // const categories = await getCategories(6);
+  const categories = await getCategories(6);
 
   return (
     <Container className="bg-shop-light-pink">
       <HomeBanner />
       <ProductGrid />
-      {/* <HomeCategories categories={categories} /> */}
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
