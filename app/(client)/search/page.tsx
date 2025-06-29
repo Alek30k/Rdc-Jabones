df4ae5738d76cb214701bc928a0f7247f5666ac2
Alek30k <alemank23k@gmail.com>
2025-06-28 21:00:09 -0300

    last commit

--- app/(client)/search/page.tsx
+++ app/(client)/search/page.tsx
@@ -29,7 +29,7 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
       </div>
 
       {products.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {products?.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
