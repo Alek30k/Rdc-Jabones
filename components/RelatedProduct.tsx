6e5bdfb81a37eea68c8ee9a7e25a9ed97250171a
Alek30k <alemank23k@gmail.com>
2025-06-29 21:58:30 -0300

    last commit

--- components/RelatedProduct.tsx
+++ components/RelatedProduct.tsx
@@ -29,7 +29,7 @@ export default function RelatedProducts({
       <h2 className="text-3xl font-bold text-center mb-8">
         También te podrían interesar
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard key={product?._id} product={product} />
         ))}
