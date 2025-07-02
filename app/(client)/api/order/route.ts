ad8c04ee8ccb3f36ec60da0f4bed3c506941f6d8
Alek30k <alemank23k@gmail.com>
2025-07-02 17:43:31 -0300

    last commit

--- app/(client)/api/order/route.ts
+++ app/(client)/api/order/route.ts
@@ -1,6 +1,6 @@
 // app/api/order/route.ts
-import { NextResponse } from "next/server";
 import { client } from "@/sanity/lib/client";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
