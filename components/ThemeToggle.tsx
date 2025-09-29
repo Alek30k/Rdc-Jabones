7a17402274e7cb1c86fe9a5dd95f017bd35109fa
Alek30k <alemank23k@gmail.com>
2025-09-28 22:00:29 -0300

    last commit

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -7,7 +7,7 @@ export function ThemeToggle() {
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="p-2 rounded-md bg-gray-200 dark:bg-gray-800"
+      className="p-2 rounded-md  "
     >
       {theme === "dark" ? "🌙" : "☀️"}
     </button>
