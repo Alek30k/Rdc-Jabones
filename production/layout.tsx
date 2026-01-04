0b08caecbe592d06c95760cf52974988a1c98f4d
Alek30k <alemank23k@gmail.com>
2026-01-04 16:54:36 -0300

    last commit

---
+++ production/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+import { ThemeProviderd } from "@/components/ThemeProviderd";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+        <ThemeProviderd attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProviderd>
+
+        <Toaster
+          position="bottom-left"
+          toastOptions={{
+            style: {
+              background: "#000000",
+              color: "#f98e34",
+            },
+          }}
+        />
+      </body>
+    </html>
+  );
+};
+export default RootLayout;
